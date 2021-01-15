6b9f4738fe67dd8df40fde2c8a80e2fa204bb0d2
Akash Levy <akashlevy@gmail.com>
2021-01-15 09:23:33 -0800

    Try again

--- gls/gls.py
+++ gls/gls.py
@@ -74,7 +74,7 @@ def convert_raw(signals, input_file, output_file):
         col[name] = i
 
     # Start with two 0000 vectors for all signals
-    #outfile.write(('_'.join(['0000']*len(signals))+'\n')*2)
+    outfile.write(('_'.join(['0000']*len(signals))+'\n')*2)
 
     # Generate vector form
     for c in range(1,len(data)):
