05a6c9c18636dc81de5c67fa994088af97473515
Akash Levy <akashlevy@gmail.com>
2021-01-15 08:38:05 -0800

    Give a one cycle boost

--- gls/gls.py
+++ gls/gls.py
@@ -73,11 +73,8 @@ def convert_raw(signals, input_file, output_file):
         # Mapping of name to column index
         col[name] = i
 
-    # Start with 0000 vector for all signals
-    outfile.write('_'.join(['0000']*len(signals))+'\n')
-
     # Generate vector form
-    for c in range(1,len(data)):
+    for c in range(2,len(data)):
         to_write = []
         # Split by signal
         for s in signals:
