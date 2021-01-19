b6e2b6d6a8b5f057be72418d7cb5b6033321927f
Akash Levy <akashlevy@gmail.com>
2021-01-18 16:12:51 -0800

    Add offset

--- gls/defines.py
+++ gls/defines.py
@@ -20,4 +20,4 @@ app_active_cycle_numbers = {
     "harris": 97,
     "pointwise": 41
 }
-app_active_cycle_numbers = {k: c + 500 for k, c in app_active_cycle_numbers.items()} # NEMS stall delay
+app_active_cycle_numbers = {k: c + 500 - 5 for k, c in app_active_cycle_numbers.items()} # NEMS stall delay
