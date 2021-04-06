87aa7d8647809892b0c0261a94bd4f5a5c153fb4
Akash Levy <akashlevy@gmail.com>
2021-04-06 00:41:00 -0700

    Revert "Revert "More changes""
    This reverts commit a8c924413f9b4561a8811a1a724f6fe337aad58e.

--- hardware/generator_z/nems/cb_unq1_decoder.v
+++
@@ -1,9 +0,0 @@
-module cb_unq1_decoder (
-    input wire [3:0] in,
-    output wire [9:0] out
-);
-
-    assign out = (1 << in);
-
-endmodule
-
--- hardware/generator_z/nems/sb_decoder_4to1_mux.v
+++
@@ -1,9 +0,0 @@
-module sb_decoder_4to1_mux (
-    input wire [1:0] in,
-    output wire [3:0] out
-);
-
-    assign out = (1 << in);
-
-endmodule
-
