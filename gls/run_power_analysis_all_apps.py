35edd45b21bc59e665548fb072ac2994b569e1c3
Michael Oduoza <mcoduoza@stanford.edu>
2021-05-04 11:28:25 -0700

    New nems_invd1 numbers

--- gls/run_power_analysis_all_apps.py
+++ gls/run_power_analysis_all_apps.py
@@ -1,14 +1,15 @@
 import os
  
 apps = ['conv_1_2', 'conv_2_1', 'conv_3_1', 'conv_bw', 'cascade', 'harris', 'pointwise']
-design_type = 'vanilla'
+design_type = 'nems'
+experiment='invd1'
 starting_dir = os.getcwd()
 
 for app in apps:
     os.system('python gls.py ' + app)
     os.system('gzip outputs/*')
     os.chdir(starting_dir + '/synopsys-ptpx-gl')
-    os.system('./run.sh ' + app + ' ' + design_type)
+    os.system('./run.sh ' + app + ' ' + design_type + ' ' + experiment)
     os.chdir(starting_dir + '/results')
     os.system('python powjson.py')
     os.chdir(starting_dir)
--- gls/synopsys-ptpx-gl/ptpx.tcl
+++ gls/synopsys-ptpx-gl/ptpx.tcl
@@ -103,9 +103,9 @@ report_power -nosplit -hierarchy -leaf \
 set underscore "_"
 
 
-report_power -nosplit -hierarchy -leaf \
-  > ../results/$designtype/$appname.power.hier.rpt
 #report_power -nosplit -hierarchy -leaf \
+  > ../results/$designtype/$appname.power.hier.rpt
+report_power -nosplit -hierarchy -leaf \
   > ../results/$designtype$underscore$experiment/$appname.power.hier.rpt
 
 exit
